f00b8683c8422cec729519819d331162b616d91c
blakelamble <blam0020@student.monash.edu>
2026-05-10 12:49:55 +1000

    Set up task 2 processing .ioc

--- Code/Task 2/Processing_STM_2/Core/Src/main.c
+++ Code/Task 2/Processing_STM_2/Core/Src/main.c
@@ -48,15 +48,19 @@ UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+static uint8_t mode = 0;                // 0 = Waiting, M = Manual, D = Distance
+static uint8_t is_recording = 0;        // Only required for distance
 
+static uint8_t buf[3] = {128, 128, 0};  // Moving Average Filter (Taken from Task 1)
+static uint8_t mean = 128;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_TIM16_Init(void);
-static void MX_USART2_UART_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_TIM16_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -95,20 +99,154 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_TIM16_Init();
-  MX_USART2_UART_Init();
   MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+  MX_TIM16_Init();
   /* USER CODE BEGIN 2 */
-  uint8_t buf[3]; // buffer of length 3
-  buf[0] = 128;
-  buf[1] = 128; // initialise at 128 for smoother start
-  uint8_t mean; // initialise mean
+ HAL_TIM_Base_Start(&htim16);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+    // Wait for python to send recording mode
+    if (mode == 0){
+        uint8_t cmd = 0;
+
+        // Every 100ms tries to receive mode command
+        if (HAL_UART_Receive(&huart2, &cmd, 1, 100) == HAL_OK){
+            // Once command has been found
+            if (cmd == 'M' || cmd == 'D'){
+                // Reset to defaults at start of new recording
+                mode = cmd;
+                is_recording = 0;
+                
+                buf[0] = 128;
+                buf[1] = 128;
+                
+                // Flushes any old bits from sampling STM 
+                uint8_t flush;
+                while (HAL_UART_Receive(&huart1, &flush, 1,  1) == HAL_OK);
+            }
+        }
+        continue;   // Restart loop - either re-check for mode, or begin using newly set mode
+    }
+
+    // Check for Stop byte from Python
+    {
+        uint8_t cmd = 0;
+
+        if (HAL_UART_Receive(&huart2, &cmd, 1, 0) == HAL_OK){
+            if (cmd == 'S'){
+                // Change mode to waiting
+                mode = 0;
+                is_recording = 0;
+                
+                // Flush sampling STM
+                uint8_t flush;
+                while (HAL_UART_Receive(&huart1, &flush, 1,  1) == HAL_OK);
+                continue;
+
+            }
+        }   
+    }
+
+    /*  
+        Manual Mode
+        Streams until python sends stop byte
+        Basically is Task 1 processing code
+    */
+    if (mode == 'M'){
+        if (HAL_UART_Receive(&huart1, &buf[2], 1, 5) == HAL_OK){
+
+            buf[0] = buf[1];
+            buf[1] = buf[2];
+            mean  = (buf[0] + buf[1]) / 2;
+
+            HAL_UART_Transmit(&huart2, &mean, 1, HAL_MAX_DELAY);
+        }
+    }
+
+    /*
+        Distance Mode
+        Streams whilst within 10cm 
+        Taken from Ultrasonic polling   
+    */
+
+    else if (mode == 'D'){
+
+    /* Trigger pulse - 10µs */
+    HAL_GPIO_WritePin(TRIG_GPIO_Port, TRIG_Pin, 1);
+    __HAL_TIM_SET_COUNTER(&htim16, 0); /* pass address of timer handle*/
+    while (__HAL_TIM_GET_COUNTER(&htim16) < 10); /* cant use hal delay, as its in ms*/
+    HAL_GPIO_WritePin(TRIG_GPIO_Port, TRIG_Pin, 0);
+    /* setting high for 10us 'triggers' the pulse*/
+
+    /* Wait for ECHO to go HIGH */
+    __HAL_TIM_SET_COUNTER(&htim16, 0); /*reset the timer again*/
+    while (HAL_GPIO_ReadPin(ECHO_GPIO_Port, ECHO_Pin) == 0) { /*Loops within timeout until echo goes high = precise timer reset*/
+        if (__HAL_TIM_GET_COUNTER(&htim16) > 30000) break; 
+    }
+
+    /* Measure echo pulse width */
+    __HAL_TIM_SET_COUNTER(&htim16, 0); /*reset timer again*/
+    while (HAL_GPIO_ReadPin(ECHO_GPIO_Port, ECHO_Pin) == 1) { /*Echo stays high until it receives signal, or timeout*/
+        if (__HAL_TIM_GET_COUNTER(&htim16) > 38000) break; /*timeout for maximum distance of the specific sensor*/
+    }
+    uint32_t echo_time = __HAL_TIM_GET_COUNTER(&htim16); /*now we can get the time taken*/
+
+    /* Distance formula: distance(cm) = echo time(µs) / 58 */
+    uint32_t distance_cm = echo_time / 58;
+    
+  /* Schmitt trigger - state check */
+  if (!is_recording && distance_cm <= 10) 
+  {
+    // OBJECT DETECTED: Flip the switch to ON
+    is_recording = 1;
+
+    // Flush bits to ensure clean recording
+    uint8_t flush;
+    while (HAL_UART_Receive(&huart1, &flush, 1,  1) == HAL_OK);
+
+    // Reset moving average filter
+    buf[0] = 128;
+    buf[1] = 128;
+  } 
+  else if (is_recording && distance_cm > 15) 
+  {
+      // OBJECT REMOVED: Flip the switch to OFF only after it clears 15cm
+      is_recording = 0;
+
+      // Send stop byte to python
+      uint8_t stop_byte = 0xFF;
+      HAL_UART_Transmit(&huart2, &stop_byte, 1, HAL_MAX_DELAY); // if we were recording, and move to far, send stop byte
+  }
+
+  /* Transfer audio if within range */
+  if (is_recording) 
+  {
+      // Changed from ultrasonic to include moving average filter from Task 1
+      // Pull from Sampling STM (UART1)
+      if (HAL_UART_Receive(&huart1, &buf[2], 1, 5) == HAL_OK) 
+      {
+        buf[0] = buf[1];
+        buf[1] = buf[2];
+        mean = (buf[0] + buf[1]) / 2;
+
+          // Forward to Python (USART2)
+          HAL_UART_Transmit(&huart2, &mean, 1, HAL_MAX_DELAY);
+      }
+      // No Delay here: Maintain high sample rate!
+  } 
+  else
+  {
+    /* Wait 60ms between readings */
+    HAL_Delay(60); //once stop byte sent, we go back to sampling every 60ms
+  }
+    
+}
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -276,13 +414,9 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(TRIG_GPIO_Port, TRIG_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : TRIG_Pin */
   GPIO_InitStruct.Pin = TRIG_Pin;
@@ -296,13 +430,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(ECHO_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LD3_Pin */
-  GPIO_InitStruct.Pin = LD3_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD3_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
